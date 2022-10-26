d6c0cf2d3388d83de8cfb8b8e167dc1b3599d911
Johnny Kristian Moeberg Sørensen <johnnykristianmoebergsoerensen@gmail.com>
2022-10-26 16:44:17 -0200

    docs: Add xml documentation

--- NetsEasyClient/Constants/ClientConstants.cs
+++ NetsEasyClient/Constants/ClientConstants.cs
@@ -1,16 +1,33 @@
-using System;
-
 namespace SolidNetsEasyClient.Constants;
 
+/// <summary>
+/// Constants for client mode
+/// </summary>
 public static class ClientConstants
 {
+    /// <summary>
+    /// The live mode client operation
+    /// </summary>
     public const string Live = "LiveNetsEasyClient";
 
+    /// <summary>
+    /// The test mode client operation
+    /// </summary>
     public const string Test = "TestNetsEasyClient";
 }
 
+/// <summary>
+/// An enumeration of client mode operations
+/// </summary>
 public enum ClientMode
 {
+    /// <summary>
+    /// Live mode
+    /// </summary>
     Live,
+
+    /// <summary>
+    /// Test mode
+    /// </summary>
     Test
 }
--- NetsEasyClient/Extensions/NetsConfigurationBuilder.cs
+++ NetsEasyClient/Extensions/NetsConfigurationBuilder.cs
@@ -8,6 +8,9 @@ using SolidNetsEasyClient.Models.Options;
 
 namespace SolidNetsEasyClient.Extensions;
 
+/// <summary>
+/// The nets configuration builder
+/// </summary>
 public sealed class NetsConfigurationBuilder
 {
     private readonly IServiceCollection services;
@@ -17,18 +20,33 @@ public sealed class NetsConfigurationBuilder
         this.services = services;
     }
 
+    /// <summary>
+    /// Add an API key from a configuration source, such as: Environment variable
+    /// </summary>
+    /// <param name="configuration">The configuration object</param>
+    /// <returns>A builder object</returns>
     public NetsConfigurationBuilder AddApiKey(IConfiguration configuration)
     {
         // TODO: load api key from configuration
         return this;
     }
 
+    /// <summary>
+    /// Add an API key directly
+    /// </summary>
+    /// <param name="apiKey">The API key to use for each request</param>
+    /// <returns>A builder object</returns>
     public NetsConfigurationBuilder AddApiKey(string apiKey)
     {
         // TODO: load api key
         return this;
     }
 
+    /// <summary>
+    /// Configure the options for the nets easy payment requests
+    /// </summary>
+    /// <param name="options">The options to set</param>
+    /// <returns>A builder object</returns>
     public NetsConfigurationBuilder Configure(Action<PlatformPaymentOptions> options)
     {
         services.Configure(options);
