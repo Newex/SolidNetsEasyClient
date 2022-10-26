f34ee6dd2db5ed716feb6055ddf8a669ff234266
Johnny Kristian Moeberg Sørensen <johnnykristianmoebergsoerensen@gmail.com>
2022-10-26 21:15:51 -0200

    feat: Reference SolidNetsEasyClient project

--- example_site/Program.cs
+++ example_site/Program.cs
@@ -1,4 +1,11 @@
+using Microsoft.AspNetCore.Builder;
+using SolidNetsEasyClient.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add nets
+builder.Services.AddNetsEasyClient();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
