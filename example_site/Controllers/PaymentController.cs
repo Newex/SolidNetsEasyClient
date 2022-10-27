f917180b7a010e2daf5e30e1474ae0ae27d7510b
Johnny Kristian Moeberg Sørensen <johnnykristianmoebergsoerensen@gmail.com>
2022-10-27 00:50:25 -0200

    wip: With minimal payment example

--- example_site/Controllers/PaymentController.cs
+++ example_site/Controllers/PaymentController.cs
@@ -1,6 +1,11 @@
 using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SolidNetsEasyClient.Clients;
+using SolidNetsEasyClient.Models;
+using SolidNetsEasyClient.Models.Requests;
 
 namespace ExampleSite.Controllers;
 
@@ -14,8 +19,35 @@ public class PaymentController : Controller
     }
 
     [HttpGet("/pay")]
-    public IActionResult Pay()
+    public async Task<ActionResult> Pay(CancellationToken cts)
     {
+        var payment = await paymentClient.CreatePaymentAsync(MinimalPaymentExample, cts);
         return View();
     }
+
+    public static PaymentRequest MinimalPaymentExample => new()
+    {
+        Order = new Order
+        {
+            Currency = "DKK",
+            Items = new List<Item>
+                {
+                    new()
+                    {
+                        Name = "Base service",
+                        Quantity = 12,
+                        Unit = "month",
+                        UnitPrice = 80000,
+                        Reference = Guid.NewGuid().ToString()
+                    }
+                },
+            Reference = Guid.NewGuid().ToString()
+        },
+        Checkout = new()
+        {
+            Url = "https://my.checkout.url",
+            TermsUrl = "https://my.terms.url",
+            ReturnUrl = "https://return.to.me"
+        },
+    };
 }
