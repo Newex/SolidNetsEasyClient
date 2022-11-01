7c32e7f79796500509960c3625ac023bd8cd0b6c
Johnny Kristian Moeberg Sørensen <johnnykristianmoebergsoerensen@gmail.com>
2022-11-01 18:58:32 -0300

    feat: Use hidden input for payment ID and checkout key

--- example_site/Controllers/CheckoutController.cs
+++ example_site/Controllers/CheckoutController.cs
@@ -20,20 +20,11 @@ public class CheckoutController : Controller
     public async Task<ActionResult> Index(BasketViewModel basket, CancellationToken cts)
     {
         var payment = await client.CreatePaymentAsync(PaymentRequestHelper.MinimalOrderExample(basket.Item, basket.Quantity), Integration.EmbeddedCheckout, cts);
-        return RedirectToAction("Pay", new
-        {
-            paymentId = payment.PaymentId.ToString("N"),
-        });
-    }
-
-    [HttpGet("/checkout")]
-    public ActionResult Pay([FromQuery] string paymentId)
-    {
         var vm = new CheckoutViewModel
         {
             CheckoutKey = client.CheckoutKey,
+            PaymentID = payment.PaymentId.ToString("N")
         };
-
         return View(vm);
     }
 }
