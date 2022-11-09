92295d2c60818a866598bba548fbd8c9185b085a
Johnny Kristian Moeberg Sørensen <johnnykristianmoebergsoerensen@gmail.com>
2022-11-09 13:27:53 -0300

    wip: Add webhook charged created

---
+++ NetsEasyClient/Models/WebHooks/ChargedCreated.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using SolidNetsEasyClient.Converters;
+
+namespace SolidNetsEasyClient.Models.WebHooks;
+
+/// <summary>
+/// The payment.charge.created.v2 event is triggered when the customer has successfully been charged, partially or fully. A use case can be to get notified for successful subscription or unscheduled charges.
+/// </summary>
+public record ChargedCreated : Webhook<ChargedData> { }
+
+/// <summary>
+/// The data associated with this event.
+/// </summary>
+public record ChargedData
+{
+    /// <summary>
+    /// The payment identifier.
+    /// </summary>
+    [JsonConverter(typeof(GuidTypeConverter))]
+    [JsonPropertyName("paymentId")]
+    public Guid PaymentId { get; init; }
+
+    /// <summary>
+    /// The subscription identifier.
+    /// </summary>
+    [JsonConverter(typeof(GuidTypeConverter))]
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    [JsonPropertyName("subscriptionId")]
+    public Guid? SubscriptionId { get; init; }
+
+    /// <summary>
+    /// The charge identifier.
+    /// </summary>
+    [JsonConverter(typeof(GuidTypeConverter))]
+    [JsonPropertyName("chargeId")]
+    public Guid ChargeId { get; init; }
+
+    /// <summary>
+    /// The list of order items that are associated with the charge. Contains at least one order item.
+    /// </summary>
+    [Required]
+    [JsonPropertyName("orderItems")]
+    public IList<Item> OrderItems { get; init; } = new List<Item>();
+
+    /// <summary>
+    /// The payment method, for example 'Visa' or 'Mastercard'.
+    /// </summary>
+    [JsonPropertyName("paymentMethod")]
+    public PaymentMethodConfigurationType PaymentMethod { get;init; }
+
+    /// <summary>
+    /// The type of payment. Possible values are: 'CARD', 'INVOICE', 'A2A', 'INSTALLMENT', 'WALLET', and 'PREPAID-INVOICE'.
+    /// </summary>
+    [JsonPropertyName("paymentType")]
+    public PaymentMethodConfigurationType PaymentType { get; init; }
+
+    /// <summary>
+    /// The amount of the charge.
+    /// </summary>
+    [JsonPropertyName("amount")]
+    public WebhookAmount Amount { get; init; } = new();
+}
--- NetsEasyClient/Models/WebHooks/PaymentCreated.cs
+++ NetsEasyClient/Models/WebHooks/PaymentCreated.cs
@@ -9,45 +9,7 @@ namespace SolidNetsEasyClient.Models.WebHooks;
 /// <summary>
 /// The <see cref="EventNames.Payment.PaymentCreated"/> event is triggered when a new payment is created. This happens when the customer hits the "Pay" button on the checkout page.
 /// </summary>
-public record PaymentCreated
-{
-    /// <summary>
-    /// A unique identifier of this event. You can use this identifier to detect whether this event is new or has already been handled by you.
-    /// </summary>
-    [JsonConverter(typeof(GuidTypeConverter))]
-    [JsonPropertyName("id")]
-    [Required]
-    public Guid Id { get; init; }
-
-    /// <summary>
-    /// The merchant number.
-    /// </summary>
-    [Required]
-    [JsonPropertyName("merchantId")]
-    public int MerchantId { get; init; }
-
-    /// <summary>
-    /// The time at which the event occurred formatted according to RFC339, for example 2021-03-23T15:30:55.23Z.
-    /// </summary>
-    [Required]
-    [JsonPropertyName("timestamp")]
-    [JsonConverter(typeof(DateTimeOffsetConverter))]
-    public DateTimeOffset Timestamp { get; init; }
-
-    /// <summary>
-    /// The name of the event, in this case <see cref="EventNames.Payment.PaymentCreated"/>
-    /// </summary>
-    [Required]
-    [JsonPropertyName("event")]
-    public EventNames Event { get; init; } = EventNames.Payment.PaymentCreated;
-
-    /// <summary>
-    /// The data associated with this event.
-    /// </summary>
-    [Required]
-    [JsonPropertyName("data")]
-    public CreatedData Data { get; init; } = new();
-}
+public record PaymentCreated : Webhook<CreatedData> { }
 
 /// <summary>
 /// The data associated with this event.
@@ -78,7 +40,7 @@ public record CreatedOrder
     /// The amount of the charge.
     /// </summary>
     [JsonPropertyName("amount")]
-    public CreatedAmount Amount { get; init; } = new();
+    public WebhookAmount Amount { get; init; } = new();
 
     /// <summary>
     /// A reference to recognize this order. Usually a number sequence (order number).
@@ -92,24 +54,4 @@ public record CreatedOrder
     [Required]
     [JsonPropertyName("orderItems")]
     public IList<Item> OrderItems { get; init; } = new List<Item>();
-}
-
-/// <summary>
-/// The amount of the charge.
-/// </summary>
-public record CreatedAmount
-{
-    /// <summary>
-    /// The amount, for example 10000
-    /// </summary>
-    [Required]
-    [JsonPropertyName("amount")]
-    public int Amount { get; init; }
-
-    /// <summary>
-    /// The currency, for example 'SEK'.
-    /// </summary>
-    [Required]
-    [JsonPropertyName("currency")]
-    public string Currency { get; init; } = string.Empty;
 }
