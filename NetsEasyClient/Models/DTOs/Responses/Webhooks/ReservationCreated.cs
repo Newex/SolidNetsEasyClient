3bb7e00fd006f22315c136cb11a6b5b7a41eb4ee
Johnny Kristian Moeberg Sørensen <johnnykristianmoebergsoerensen@gmail.com>
2022-11-10 01:01:23 -0300

    refactor: Extract webhook order

--- NetsEasyClient/Models/DTOs/Responses/Webhooks/ReservationCreated.cs
+++ NetsEasyClient/Models/DTOs/Responses/Webhooks/ReservationCreated.cs
@@ -2,22 +2,17 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 using SolidNetsEasyClient.Converters;
-using SolidNetsEasyClient.Models.DTOs.Enums;
 
 namespace SolidNetsEasyClient.Models.DTOs.Responses.Webhooks;
 
-/// <summary>
-/// The <see cref="EventName.PaymentCreated"/> event is triggered when a new payment is created. This happens when the customer hits the "Pay" button on the checkout page.
-/// </summary>
-public record PaymentCreated : Webhook<CreatedData> { }
+public record ReservationCreated : Webhook<ReservationCreatedData>
+{
+}
 
-/// <summary>
-/// The data associated with this event.
-/// </summary>
-public record CreatedData
+public record ReservationCreatedData
 {
     /// <summary>
-    /// The payment identifier.
+    /// The payment identifier
     /// </summary>
     [JsonConverter(typeof(GuidTypeConverter))]
     [JsonPropertyName("paymentId")]
