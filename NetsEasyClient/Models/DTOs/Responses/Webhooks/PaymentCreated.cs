a869e50506702479a8a4a9e812773c292bfa8515
Johnny Kristian Moeberg Sørensen <johnnykristianmoebergsoerensen@gmail.com>
2022-11-09 22:58:18 -0300

    docs: Fix event enum name

--- NetsEasyClient/Models/DTOs/Responses/Webhooks/PaymentCreated.cs
+++ NetsEasyClient/Models/DTOs/Responses/Webhooks/PaymentCreated.cs
@@ -8,7 +8,7 @@ using SolidNetsEasyClient.Models.DTOs.Enums;
 namespace SolidNetsEasyClient.Models.DTOs.Responses.Webhooks;
 
 /// <summary>
-/// The <see cref="EventNames.Payment.PaymentCreated"/> event is triggered when a new payment is created. This happens when the customer hits the "Pay" button on the checkout page.
+/// The <see cref="EventName.PaymentCreated"/> event is triggered when a new payment is created. This happens when the customer hits the "Pay" button on the checkout page.
 /// </summary>
 public record PaymentCreated : Webhook<CreatedData> { }
 
