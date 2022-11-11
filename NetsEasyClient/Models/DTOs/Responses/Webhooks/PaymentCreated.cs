41b633968b14e3ef9713b7abb17284c2f5c0d593
Johnny Kristian Moeberg Sørensen <johnnykristianmoebergsoerensen@gmail.com>
2022-11-11 14:46:15 -0300

    refactor: Extract payment created payload

--- NetsEasyClient/Models/DTOs/Responses/Webhooks/PaymentCreated.cs
+++ NetsEasyClient/Models/DTOs/Responses/Webhooks/PaymentCreated.cs
@@ -1,33 +1,9 @@
-using System;
-using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
-using SolidNetsEasyClient.Converters;
 using SolidNetsEasyClient.Models.DTOs.Enums;
-using SolidNetsEasyClient.Models.DTOs.Responses.Webhooks.Common;
+using SolidNetsEasyClient.Models.DTOs.Responses.Webhooks.Payloads;
 
 namespace SolidNetsEasyClient.Models.DTOs.Responses.Webhooks;
 
 /// <summary>
 /// The <see cref="EventName.PaymentCreated"/> event is triggered when a new payment is created. This happens when the customer hits the "Pay" button on the checkout page.
 /// </summary>
-public record PaymentCreated : Webhook<CreatedData> { }
-
-/// <summary>
-/// The data associated with this event.
-/// </summary>
-public record CreatedData
-{
-    /// <summary>
-    /// The payment identifier.
-    /// </summary>
-    [JsonConverter(typeof(GuidTypeConverter))]
-    [JsonPropertyName("paymentId")]
-    public Guid PaymentId { get; init; }
-
-    /// <summary>
-    /// The complete order associated with the payment.
-    /// </summary>
-    [Required]
-    [JsonPropertyName("order")]
-    public WebhookOrder Order { get; init; } = new();
-}
+public record PaymentCreated : Webhook<PaymentCreatedData> { }
