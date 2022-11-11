a62fa990c5599d7d421cc0ba75696aacab201733
Johnny Kristian Moeberg Sørensen <johnnykristianmoebergsoerensen@gmail.com>
2022-11-11 14:50:18 -0300

    refactor: Extract refund failed payload

--- NetsEasyClient/Models/DTOs/Responses/Webhooks/RefundFailed.cs
+++ NetsEasyClient/Models/DTOs/Responses/Webhooks/RefundFailed.cs
@@ -1,8 +1,4 @@
-using System;
-using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
-using SolidNetsEasyClient.Converters;
-using SolidNetsEasyClient.Models.DTOs.Responses.Webhooks.Common;
+using SolidNetsEasyClient.Models.DTOs.Responses.Webhooks.Payloads;
 
 namespace SolidNetsEasyClient.Models.DTOs.Responses.Webhooks;
 
@@ -10,45 +6,3 @@ namespace SolidNetsEasyClient.Models.DTOs.Responses.Webhooks;
 /// The payment.refund.failed event is triggered when a refund attempt has failed.
 /// </summary>
 public record RefundFailed : Webhook<RefundFailedData> { }
-
-/// <summary>
-/// The refund failed payload data
-/// </summary>
-public record RefundFailedData
-{
-    /// <summary>
-    /// The payment identifier
-    /// </summary>
-    [Required]
-    [JsonConverter(typeof(GuidTypeConverter))]
-    [JsonPropertyName("paymentId")]
-    public Guid PaymentId { get; init; }
-
-    /// <summary>
-    /// A unique identifier of this refund.
-    /// </summary>
-    [Required]
-    [JsonConverter(typeof(GuidTypeConverter))]
-    [JsonPropertyName("refundId")]
-    public Guid RefundId { get; init; }
-
-    /// <summary>
-    /// Contains information about an error (client error or server error).
-    /// </summary>
-    [Required]
-    [JsonPropertyName("error")]
-    public WebhookError Error { get; init; } = new();
-
-    /// <summary>
-    /// The amount of the refund.
-    /// </summary>
-    [Required]
-    [JsonPropertyName("amount")]
-    public WebhookAmount Amount { get; init; } = new();
-
-    /// <summary>
-    /// The invoice details
-    /// </summary>
-    [JsonPropertyName("invoiceDetails")]
-    public WebhookInvoiceDetails InvoiceDetails { get; init; } = new();
-}
