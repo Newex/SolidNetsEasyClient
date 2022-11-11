4426286044e0c02d7932c2aabfe830ebb514ee68
Johnny Kristian Moeberg Sørensen <johnnykristianmoebergsoerensen@gmail.com>
2022-11-11 15:01:52 -0300

    refactor: Extract reservation failed payload

--- NetsEasyClient/Models/DTOs/Responses/Webhooks/ReservationFailed.cs
+++ NetsEasyClient/Models/DTOs/Responses/Webhooks/ReservationFailed.cs
@@ -1,9 +1,5 @@
-using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
-using SolidNetsEasyClient.Converters;
-using SolidNetsEasyClient.Models.DTOs.Responses.Webhooks.Common;
+using SolidNetsEasyClient.Models.DTOs.Responses.Webhooks.Payloads;
 
 namespace SolidNetsEasyClient.Models.DTOs.Responses.Webhooks;
 
@@ -20,36 +16,4 @@ public record ReservationFailed : Webhook<ReservationFailedData>
         get { return Data.OrderItems; }
         init { Data.OrderItems = value; }
     }
-}
-
-/// <summary>
-/// The failed reservation data
-/// </summary>
-public record ReservationFailedData
-{
-    /// <summary>
-    /// The payment identifier.
-    /// </summary>
-    [Required]
-    [JsonConverter(typeof(GuidTypeConverter))]
-    [JsonPropertyName("paymentId")]
-    public Guid PaymentId { get; init; }
-
-    /// <summary>
-    /// The amount of the charge.
-    /// </summary>
-    [JsonPropertyName("amount")]
-    public WebhookAmount Amount { get; init; } = new();
-
-    /// <summary>
-    /// Order items as in the example payload
-    /// </summary>
-    [JsonPropertyName("orderItems")]
-    public IList<Item> OrderItems { get; set; } = new List<Item>();
-
-    /// <summary>
-    /// The error details. (From example)
-    /// </summary>
-    [JsonPropertyName("error")]
-    public WebhookError Error { get; init; } = new();
 }
