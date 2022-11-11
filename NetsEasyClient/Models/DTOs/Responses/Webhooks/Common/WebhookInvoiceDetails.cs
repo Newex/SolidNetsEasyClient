a18bedaf1d75a6cfaa0c4bc182a1bac0d542853d
Johnny Kristian Moeberg Sørensen <johnnykristianmoebergsoerensen@gmail.com>
2022-11-11 14:24:22 -0300

    refactor: Move InvoiceDetails to Common

--- NetsEasyClient/Models/DTOs/Responses/Webhooks/Common/WebhookInvoiceDetails.cs
+++ NetsEasyClient/Models/DTOs/Responses/Webhooks/Common/WebhookInvoiceDetails.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 using SolidNetsEasyClient.Converters;
 
-namespace SolidNetsEasyClient.Models.DTOs.Responses.Webhooks;
+namespace SolidNetsEasyClient.Models.DTOs.Responses.Webhooks.Common;
 
 /// <summary>
 /// Invoice details
