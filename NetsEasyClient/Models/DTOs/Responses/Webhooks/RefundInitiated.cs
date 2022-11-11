ae2eed551e34f7f1b1b33b897fc915d4b958568c
Johnny Kristian Moeberg Sørensen <johnnykristianmoebergsoerensen@gmail.com>
2022-11-11 14:52:29 -0300

    refactor: Extract refund initiated payload

--- NetsEasyClient/Models/DTOs/Responses/Webhooks/RefundInitiated.cs
+++ NetsEasyClient/Models/DTOs/Responses/Webhooks/RefundInitiated.cs
@@ -1,8 +1,6 @@
-using System;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
-using SolidNetsEasyClient.Converters;
-using SolidNetsEasyClient.Models.DTOs.Responses.Webhooks.Common;
+using SolidNetsEasyClient.Models.DTOs.Responses.Webhooks.Payloads;
 
 namespace SolidNetsEasyClient.Models.DTOs.Responses.Webhooks;
 
@@ -21,41 +19,4 @@ public record RefundInitiated : Webhook<RefundInitiatedData>
         get { return MerchantId; }
         init { MerchantId = value; }
     }
-}
-
-/// <summary>
-/// The refund initiated payload
-/// </summary>
-public record RefundInitiatedData
-{
-    /// <summary>
-    /// The payment identifier
-    /// </summary>
-    [Required]
-    [JsonConverter(typeof(GuidTypeConverter))]
-    [JsonPropertyName("paymentId")]
-    public Guid PaymentId { get; init; }
-
-    /// <summary>
-    /// A unique identifier of this refund.
-    /// </summary>
-    [Required]
-    [JsonConverter(typeof(GuidTypeConverter))]
-    [JsonPropertyName("refundId")]
-    public Guid RefundId { get; init; }
-
-    /// <summary>
-    /// The charge identifier.
-    /// </summary>
-    [Required]
-    [JsonConverter(typeof(GuidTypeConverter))]
-    [JsonPropertyName("chargeId")]
-    public Guid ChargeId { get; init; }
-
-    /// <summary>
-    /// The amount of the refund.
-    /// </summary>
-    [Required]
-    [JsonPropertyName("amount")]
-    public WebhookAmount Amount { get; init; } = new();
 }
