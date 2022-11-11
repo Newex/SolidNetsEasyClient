ee666535934688ddc0cff91c1816cd152aa3f91c
Johnny Kristian Moeberg Sørensen <johnnykristianmoebergsoerensen@gmail.com>
2022-11-11 14:42:46 -0300

    refactor: Extract payment cancelled failed into payload

--- NetsEasyClient/Models/DTOs/Responses/Webhooks/PaymentCancelledFailed.cs
+++ NetsEasyClient/Models/DTOs/Responses/Webhooks/PaymentCancelledFailed.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
-using SolidNetsEasyClient.Converters;
-using SolidNetsEasyClient.Models.DTOs.Responses.Webhooks.Common;
+using SolidNetsEasyClient.Models.DTOs.Responses.Webhooks.Payloads;
 
 namespace SolidNetsEasyClient.Models.DTOs.Responses.Webhooks;
 
@@ -14,46 +9,3 @@ namespace SolidNetsEasyClient.Models.DTOs.Responses.Webhooks;
 /// The payment.cancel.failed event is triggered when a cancellation of a reservation has failed.
 /// </remarks>
 public record PaymentCancelledFailed : Webhook<PaymentCancelledFailedData> { }
-
-/// <summary>
-/// The data associated with this event.
-/// </summary>
-public record PaymentCancelledFailedData
-{
-    /// <summary>
-    /// The payment identifier.
-    /// </summary>
-    [Required]
-    [JsonConverter(typeof(GuidTypeConverter))]
-    [JsonPropertyName("paymentId")]
-    public Guid PaymentId { get; init; }
-
-    /// <summary>
-    /// Contains information about an error (client error or server error).
-    /// </summary>
-    [Required]
-    [JsonPropertyName("error")]
-    public WebhookError Error { get; init; } = new();
-
-    /// <summary>
-    /// The cancellation id
-    /// </summary>
-    [Required]
-    [JsonConverter(typeof(GuidTypeConverter))]
-    [JsonPropertyName("cancelId")]
-    public Guid CancelId { get; init; }
-
-    /// <summary>
-    /// The list of order items that are associated with the charge. Contains at least one order item.
-    /// </summary>
-    [Required]
-    [JsonPropertyName("orderItems")]
-    public IList<Item> OrderItems { get; init; } = new List<Item>();
-
-    /// <summary>
-    /// The amount of the charge.
-    /// </summary>
-    [Required]
-    [JsonPropertyName("amount")]
-    public WebhookAmount Amount { get; init; } = new();
-}
