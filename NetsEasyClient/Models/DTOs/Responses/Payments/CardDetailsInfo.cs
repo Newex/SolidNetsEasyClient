4f00580141b669106ea491fd4e4dbac8437bf71f
Johnny Kristian Moeberg Sørensen <johnnykristianmoebergsoerensen@gmail.com>
2022-11-15 17:21:26 -0300

    wip: Make CardDetailsInfo properties required

--- NetsEasyClient/Models/DTOs/Responses/Payments/CardDetailsInfo.cs
+++ NetsEasyClient/Models/DTOs/Responses/Payments/CardDetailsInfo.cs
@@ -10,14 +10,12 @@ public record CardDetailsInfo
     /// <summary>
     /// A masked version of the PAN (Primary Account Number). At maximum, only the first six and last four digits of the account number are displayed
     /// </summary>
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     [JsonPropertyName("maskedPan")]
-    public string? MaskedPan { get; init; }
+    public string MaskedPan { get; init; } = string.Empty;
 
     /// <summary>
     /// The four-digit expiration date of the payment card. The format should be: MMYY
     /// </summary>
-    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     [JsonPropertyName("expiryDate")]
-    public MonthOnly? ExpiryDate { get; init; }
+    public MonthOnly ExpiryDate { get; init; }
 }
