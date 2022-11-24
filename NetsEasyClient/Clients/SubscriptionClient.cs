9b6e5d7c1cfa5fc17c452e24cd6900ee1b5f5269
Johnny Kristian Moeberg Sørensen <johnnykristianmoebergsoerensen@gmail.com>
2022-11-23 22:23:46 -0300

    refactor: Extract interface

--- NetsEasyClient/Clients/SubscriptionClient.cs
+++ NetsEasyClient/Clients/SubscriptionClient.cs
@@ -21,13 +21,8 @@ using SolidNetsEasyClient.Validators;
 
 namespace SolidNetsEasyClient.Clients;
 
-/// <summary>
-/// Subscription client, responsible for mediating communication between NETS payment API and this client
-/// </summary>
-/// <remarks>
-/// Subscriptions allow you to charge your customers on a regular basis, for example a monthly subscription for a product the customer must pay for every month. When a subscription is charged, a new payment object is created to represent the purchase of the subscription product. It is possible to verify and charge multiple subscriptions in bulk using the Bulk charge subscriptions method.
-/// </remarks>
-public class SubscriptionClient
+/// <inheritdoc />
+public class SubscriptionClient : ISubscriptionClient
 {
     private readonly IHttpClientFactory httpClientFactory;
     private readonly ILogger<SubscriptionClient> logger;
@@ -58,13 +53,7 @@ public class SubscriptionClient
         apiKey = options.Value.ApiKey;
     }
 
-    /// <summary>
-    /// Retrieves an existing subscription by a subscriptionId. The subscriptionId can be obtained from the Retrieve payment method.
-    /// </summary>
-    /// <param name="subscriptionId">The subscription identifier (a UUID).</param>
-    /// <param name="cancellationToken">The cancellation token</param>
-    /// <returns>Details of a subscription</returns>
-    /// <exception cref="ArgumentException">Thrown if subscription id is empty</exception>
+    /// <inheritdoc />
     public async Task<SubscriptionDetails> RetrieveSubscriptionAsync(Guid subscriptionId, CancellationToken cancellationToken)
     {
         var isValid = subscriptionId != Guid.Empty;
@@ -92,20 +81,14 @@ public class SubscriptionClient
             logger.LogInformation("Retrieved {@Subscription}", result);
             return result;
         }
-        catch(Exception ex)
+        catch (Exception ex)
         {
             logger.LogError(ex, "An exception occurred trying to retrieve subscription with {@SubscriptionID}", subscriptionId);
             throw;
         }
     }
 
-    /// <summary>
-    /// Retrieves a subscription matching the specified externalReference. This method can only be used for retrieving subscriptions that have been imported from a payment platform other than Nets Easy. Subscriptions created within Nets Easy do not have an externalReference value set.
-    /// </summary>
-    /// <param name="externalReference">The external reference to search for.</param>
-    /// <param name="cancellationToken">The cancellation token</param>
-    /// <returns>Details of a subscription</returns>
-    /// <exception cref="ArgumentException">Thrown if the external reference is null or only contains whitespaces</exception>
+    /// <inheritdoc />
     public async Task<SubscriptionDetails> RetrieveSubscriptionByExternalReferenceAsync(string externalReference, CancellationToken cancellationToken)
     {
         var isValid = !string.IsNullOrWhiteSpace(externalReference);
@@ -133,22 +116,14 @@ public class SubscriptionClient
             logger.LogInformation("Retrieved {@Subscription}", result);
             return result;
         }
-        catch(Exception ex)
+        catch (Exception ex)
         {
             logger.LogError(ex, "An exception occurred trying to retrieve subscription with {@ExternalReference}", externalReference);
             throw;
         }
     }
 
-    /// <summary>
-    /// Charges multiple subscriptions at once. The request body must contain: (*) A unique string that identifies this bulk charge operation. (*) A set of subscription identifiers that should be charged. To get status updates about the bulk charge you can subscribe to the webhooks for charges and refunds (payment.charges.* and payments.refunds.*). See also the webhooks documentation.
-    /// </summary>
-    /// <param name="subscriptions">The list of subscriptions to charge in bulk</param>
-    /// <param name="cancellationToken">The cancellation token</param>
-    /// <param name="externalBulkChargeId">A string that uniquely identifies the bulk charge operation. Use this property for enabling safe retries. Must be between 1 and 64 characters.</param>
-    /// <param name="notifications">Subscribe to notifications</param>
-    /// <returns>A bulk id</returns>
-    /// <exception cref="ArgumentException">Thrown if subscriptions or external bulk charge id is invalid</exception>
+    /// <inheritdoc />
     public async Task<BulkId> BulkChargeSubscriptionsAsync(IList<SubscriptionCharge> subscriptions, CancellationToken cancellationToken, string? externalBulkChargeId = null, Notification? notifications = null)
     {
         // Subscription must either have a subscriptionId or an externalReference but not both!
@@ -173,7 +148,7 @@ public class SubscriptionClient
                 Subscriptions = subscriptions
             };
             var response = await client.PostAsJsonAsync(path, bulk, cancellationToken);
-            response.EnsureSuccessStatusCode();
+            _ = response.EnsureSuccessStatusCode();
             var result = await response.Content.ReadFromJsonAsync<BulkId>();
             if (result is null)
             {
@@ -183,19 +158,14 @@ public class SubscriptionClient
             logger.LogInformation("Bulk charge id: {@BulkChargeId}", result);
             return result;
         }
-        catch(Exception ex)
+        catch (Exception ex)
         {
             logger.LogError(ex, "An exception occurred trying to bulk charge {@Subscriptions}", subscriptions);
             throw;
         }
     }
 
-    /// <summary>
-    /// Retrieves charges associated with the specified bulk charge operation. The bulkId is returned from Nets in the response of the Bulk charge subscriptions method.
-    /// </summary>
-    /// <param name="bulkId">The identifier of the bulk charge operation that was returned from the Bulk charge subscriptions method.</param>
-    /// <param name="cancellationToken">The cancellation token</param>
-    /// <returns>A paginated set of subscriptions</returns>
+    /// <inheritdoc />
     public async Task<PaginatedSubscriptions> RetrieveBulkChargesAsync(Guid bulkId, CancellationToken cancellationToken)
     {
         try
@@ -204,7 +174,7 @@ public class SubscriptionClient
             var response = await RetrieveBulkChargesAsync(bulkId, null, null, null, null, cancellationToken);
             var msg = await response.Content.ReadAsStringAsync(cancellationToken);
             logger.LogTrace("Content is: {@MessageContent}", msg);
-            response.EnsureSuccessStatusCode();
+            _ = response.EnsureSuccessStatusCode();
 
             var result = JsonSerializer.Deserialize<PaginatedSubscriptions>(msg);
             if (result is null)
@@ -216,21 +186,14 @@ public class SubscriptionClient
             logger.LogInformation("Retrieved bulk subscriptions: {@PaginatedSubscriptions}", result);
             return result;
         }
-        catch(Exception ex)
+        catch (Exception ex)
         {
             logger.LogError(ex, "An exception occurred trying to bulk retrieve {@BulkId}", bulkId);
             throw;
         }
     }
 
-    /// <summary>
-    /// Retrieves charges associated with the specified bulk charge operation. The bulkId is returned from Nets in the response of the Bulk charge subscriptions method. This method supports pagination. Specify the range of subscriptions to retrieve by using either skip and take. The boolean property named more in the response body, indicates whether there are more subscriptions beyond the requested range.
-    /// </summary>
-    /// <param name="bulkId">The bulk id</param>
-    /// <param name="skip">The number of subscription entries to skip from the start. Use this property in combination with the take property.</param>
-    /// <param name="take">The maximum number of subscriptions to be retrieved. Use this property in combination with the skip property.</param>
-    /// <param name="cancellationToken">The cancellation token</param>
-    /// <returns>A paginated set of subscriptions</returns>
+    /// <inheritdoc />
     public async Task<PaginatedSubscriptions> RetrieveBulkChargesAsync(Guid bulkId, int skip, int take, CancellationToken cancellationToken)
     {
         try
@@ -239,7 +202,7 @@ public class SubscriptionClient
             var response = await RetrieveBulkChargesAsync(bulkId, skip, take, null, null, cancellationToken);
             var msg = await response.Content.ReadAsStringAsync(cancellationToken);
             logger.LogTrace("Content is: {@MessageContent}", msg);
-            response.EnsureSuccessStatusCode();
+            _ = response.EnsureSuccessStatusCode();
 
             var result = JsonSerializer.Deserialize<PaginatedSubscriptions>(msg);
             if (result is null)
@@ -251,21 +214,14 @@ public class SubscriptionClient
             logger.LogInformation("Retrieved bulk subscriptions: {@PaginatedSubscriptions}", result);
             return result;
         }
-        catch(Exception ex)
+        catch (Exception ex)
         {
             logger.LogError(ex, "An exception occurred trying to bulk retrieve {@BulkId}", bulkId);
             throw;
         }
     }
 
-    /// <summary>
-    /// Retrieves charges associated with the specified bulk charge operation. The bulkId is returned from Nets in the response of the Bulk charge subscriptions method. This method supports pagination. Specify the range of subscriptions to retrieve by using pageNumber together with pageSize. The boolean property named more in the response body, indicates whether there are more subscriptions beyond the requested range.
-    /// </summary>
-    /// <param name="bulkId">The identifier of the bulk charge operation that was returned from the Bulk charge subscriptions method.</param>
-    /// <param name="pageSize">The size of each page when specify the range of subscriptions using the pageNumber property.</param>
-    /// <param name="pageNumber">The page number to be retrieved. Use this property in combination with the pageSize property.</param>
-    /// <param name="cancellationToken">The cancellation token</param>
-    /// <returns>A paginated set of subscriptions</returns>
+    /// <inheritdoc />
     public async Task<PaginatedSubscriptions> RetrieveBulkChargesAsync(Guid bulkId, int pageSize, ushort pageNumber, CancellationToken cancellationToken)
     {
         try
@@ -274,7 +230,7 @@ public class SubscriptionClient
             var response = await RetrieveBulkChargesAsync(bulkId, null, null, pageSize, pageNumber, cancellationToken);
             var msg = await response.Content.ReadAsStringAsync(cancellationToken);
             logger.LogTrace("Content is: {@MessageContent}", msg);
-            response.EnsureSuccessStatusCode();
+            _ = response.EnsureSuccessStatusCode();
 
             var result = JsonSerializer.Deserialize<PaginatedSubscriptions>(msg);
             if (result is null)
@@ -286,20 +242,14 @@ public class SubscriptionClient
             logger.LogInformation("Retrieved bulk subscriptions: {@PaginatedSubscriptions}", result);
             return result;
         }
-        catch(Exception ex)
+        catch (Exception ex)
         {
             logger.LogError(ex, "An exception occurred trying to bulk retrieve {@BulkId}", bulkId);
             throw;
         }
     }
 
-    /// <summary>
-    /// Verifies the specified set of subscriptions in bulk. The bulkId returned from a successful request can be used for querying the status of the subscriptions.
-    /// </summary>
-    /// <param name="verifications">The set of subscriptions that should be verified. Each item in the array should define either a subscriptioId or an externalReference, but not both.</param>
-    /// <param name="cancellationToken">The cancellation token</param>
-    /// <returns>A bulk id</returns>
-    /// <exception cref="ArgumentException">Thrown if invalid subscriptions or external bulk verification id</exception>
+    /// <inheritdoc />
     public async Task<BulkId> VerifyBulkSubscriptionsAsync(BulkSubscriptionVerification verifications, CancellationToken cancellationToken)
     {
         var isValid = (verifications.ExternalBulkVerificationId is null || (verifications.ExternalBulkVerificationId?.Length > 1 && verifications.ExternalBulkVerificationId.Length < 64))
@@ -320,7 +270,7 @@ public class SubscriptionClient
             var response = await client.PostAsJsonAsync(path, verifications, cancellationToken);
             var msg = await response.Content.ReadAsStringAsync(cancellationToken);
             logger.LogTrace("Content is: {@MessageContent}", msg);
-            response.EnsureSuccessStatusCode();
+            _ = response.EnsureSuccessStatusCode();
             var result = JsonSerializer.Deserialize<BulkId>(msg);
             if (result is null)
             {
@@ -331,31 +281,21 @@ public class SubscriptionClient
             logger.LogInformation("Verified {@Subscriptions}, with {@BulkId}", verifications, result);
             return result;
         }
-        catch(Exception ex)
+        catch (Exception ex)
         {
             logger.LogError(ex, "An exception occurred trying to verify bulk subscriptions of {@Bulk}", verifications);
             throw;
         }
     }
 
-    /// <summary>
-    /// Retrieves verifications associated with the specified bulk verification operation. The bulkId is returned from Nets in the response of the Verify subscriptions method. This method supports pagination. Specify the range of subscriptions to retrieve by using either skip and take or pageNumber together with pageSize. The boolean property named more in the response body, indicates whether there are more subscriptions beyond the requested range.
-    /// </summary>
-    /// <param name="bulkId">The identifier of the bulk verification operation that was returned from the erfiy subscriptions method.</param>
-    /// <param name="skip">The number of subscription entries to skip from the start. Use this property in combination with the take property.</param>
-    /// <param name="take">The maximum number of subscriptions to be retrieved. Use this property in combination with the skip property.</param>
-    /// <param name="pageNumber">The page number to be retrieved. Use this property in combination with the pageSize property.</param>
-    /// <param name="pageSize">The size of each page when specify the range of subscriptions using the pageNumber property.</param>
-    /// <param name="cancellationToken">The cancellation token</param>
-    /// <returns>A paginated result set of subscriptions</returns>
-    /// <exception cref="ArgumentException">Thrown if bulk id is empty or if skip, take, pageNumber or pageSize is negative</exception>
+    /// <inheritdoc />
     public async Task<PaginatedSubscriptions> RetrieveBulkVerificationsAsync(Guid bulkId, int? skip, int? take, int? pageNumber, int? pageSize, CancellationToken cancellationToken)
     {
         bool isValid = bulkId != Guid.Empty;
-        isValid &= skip is null || skip > 0;
-        isValid &= take is null || take > 0;
-        isValid &= pageNumber is null || pageNumber > 0;
-        isValid &= pageSize is null || pageSize > 0;
+        isValid &= skip is null or > 0;
+        isValid &= take is null or > 0;
+        isValid &= pageNumber is null or > 0;
+        isValid &= pageSize is null or > 0;
 
         if (!isValid)
         {
@@ -378,7 +318,7 @@ public class SubscriptionClient
             var response = await client.GetAsync(path, cancellationToken);
             var msg = await response.Content.ReadAsStringAsync(cancellationToken);
             logger.LogTrace("Content is: {@MessageContent}", msg);
-            response.EnsureSuccessStatusCode();
+            _ = response.EnsureSuccessStatusCode();
             var result = JsonSerializer.Deserialize<PaginatedSubscriptions>(msg);
             if (result is null)
             {
@@ -389,7 +329,7 @@ public class SubscriptionClient
             logger.LogInformation("Retrieved verified {@PaginatedSubscriptions}", result);
             return result;
         }
-        catch(Exception ex)
+        catch (Exception ex)
         {
             logger.LogError(ex, "An exception occurred trying to retrieve verifications of {@BulkId}", bulkId);
             throw;
@@ -424,15 +364,15 @@ public class SubscriptionClient
 
             if (isFirst)
             {
-                sb.Append('?');
+                _ = sb.Append('?');
                 isFirst = false;
             }
             else
             {
-                sb.Append('&');
+                _ = sb.Append('&');
             }
 
-            sb.Append(Key)
+            _ = sb.Append(Key)
                 .Append('=')
                 .Append(Value);
         }
