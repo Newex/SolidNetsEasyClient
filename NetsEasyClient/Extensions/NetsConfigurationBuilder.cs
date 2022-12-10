52aef0153bcbe085d33e569dc13918cc15e64647
Johnny Kristian Moeberg Sørensen <johnnykristianmoebergsoerensen@gmail.com>
2022-12-10 14:18:07 -0300

    feat: Add configuration options for http client and retry

--- NetsEasyClient/Extensions/NetsConfigurationBuilder.cs
+++ NetsEasyClient/Extensions/NetsConfigurationBuilder.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Net.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Polly;
 using SolidNetsEasyClient.Clients;
 using SolidNetsEasyClient.Constants;
 using SolidNetsEasyClient.Models.Options;
@@ -13,6 +15,7 @@ namespace SolidNetsEasyClient.Extensions;
 /// </summary>
 public sealed class NetsConfigurationBuilder
 {
+    // private readonly ServiceDescriptor 
     private readonly IServiceCollection services;
 
     private NetsConfigurationBuilder(IServiceCollection services)
@@ -40,6 +43,56 @@ public sealed class NetsConfigurationBuilder
     public NetsConfigurationBuilder Configure(Action<PlatformPaymentOptions> options)
     {
         services.Configure(options);
+        return this;
+    }
+
+    /// <summary>
+    /// Configure the http client for a specific mode
+    /// </summary>
+    /// <param name="mode">The client mode</param>
+    /// <param name="options">The http client options</param>
+    /// <param name="transientHttpErrorPolicy">The retry policy configuration for the Polly library</param>
+    /// <returns>A builder object</returns>
+    /// <exception cref="NotSupportedException">If client mode is not supported</exception>
+    public NetsConfigurationBuilder ConfigureHttpClient(ClientMode mode, Action<HttpClient> options, Func<PolicyBuilder<HttpResponseMessage>, IAsyncPolicy<HttpResponseMessage>>? transientHttpErrorPolicy = null)
+    {
+        var clientMode = mode switch
+        {
+            ClientMode.Live => ClientConstants.Live,
+            ClientMode.Test => ClientConstants.Test,
+            _ => throw new NotSupportedException()
+        };
+
+        if (transientHttpErrorPolicy is not null)
+        {
+            services
+                .AddHttpClient(clientMode, options)
+                .AddTransientHttpErrorPolicy(transientHttpErrorPolicy);
+        }
+        else
+        {
+            services.AddHttpClient(clientMode, options);
+        }
+        return this;
+    }
+
+    /// <summary>
+    /// Configure the http client transient retry policy for a specific mode
+    /// </summary>
+    /// <param name="mode">The client mode</param>
+    /// <param name="transientHttpErrorPolicy">The retry policy configuration using the Polly library</param>
+    /// <returns>A builder object</returns>
+    /// <exception cref="NotSupportedException">Thrown if client mode is not supported</exception>
+    public NetsConfigurationBuilder ConfigureRetryPolicy(ClientMode mode, Func<PolicyBuilder<HttpResponseMessage>, IAsyncPolicy<HttpResponseMessage>> transientHttpErrorPolicy)
+    {
+        var clientMode = mode switch
+        {
+            ClientMode.Live => ClientConstants.Live,
+            ClientMode.Test => ClientConstants.Test,
+            _ => throw new NotSupportedException()
+        };
+
+        services.AddHttpClient(clientMode).AddTransientHttpErrorPolicy(transientHttpErrorPolicy);
         return this;
     }
 
