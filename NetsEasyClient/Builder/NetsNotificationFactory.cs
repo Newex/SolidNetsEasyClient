6e567f2462e48e3af2f4d0df0d7cced95b86eb9b
Johnny Kristian Moeberg Sørensen <mojo11ku@hotmail.com>
2024-05-02 23:17:04 -0100

    refactor: Use primary constructor

--- NetsEasyClient/Builder/NetsNotificationFactory.cs
+++ NetsEasyClient/Builder/NetsNotificationFactory.cs
@@ -7,28 +7,21 @@ namespace SolidNetsEasyClient.Builder;
 /// <summary>
 /// Notifications builder factory
 /// </summary>
-public class NetsNotificationFactory
-{
-    private readonly string baseUrl;
-    private readonly WebhookEncryptionOptions webhookOptions;
-    private readonly LinkGenerator linkGenerator;
-
-    /// <summary>
-    /// Instantiate a new <see cref="NetsNotificationFactory"/>
-    /// </summary>
-    /// <param name="options">The nets easy options</param>
-    /// <param name="webhookOptions">The webhook encryption options</param>
-    /// <param name="linkGenerator">The link generator</param>
-    public NetsNotificationFactory(
-        IOptions<NetsEasyOptions> options,
-        IOptions<WebhookEncryptionOptions> webhookOptions,
-        LinkGenerator linkGenerator
+/// <remarks>
+/// Instantiate a new <see cref="NetsNotificationFactory"/>
+/// </remarks>
+/// <param name="options">The nets easy options</param>
+/// <param name="webhookOptions">The webhook encryption options</param>
+/// <param name="linkGenerator">The link generator</param>
+public class NetsNotificationFactory(
+    IOptions<NetsEasyOptions> options,
+    IOptions<WebhookEncryptionOptions> webhookOptions,
+    LinkGenerator linkGenerator
     )
-    {
-        baseUrl = options.Value.BaseUrl;
-        this.webhookOptions = webhookOptions.Value;
-        this.linkGenerator = linkGenerator;
-    }
+{
+    private readonly string baseUrl = options.Value.BaseUrl;
+    private readonly WebhookEncryptionOptions webhookOptions = webhookOptions.Value;
+    private readonly LinkGenerator linkGenerator = linkGenerator;
 
     /// <summary>
     /// Create notification builder
