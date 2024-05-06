88a4f0139bd88dac899b6cfadc5dbaef1fce2193
Johnny Kristian Moeberg Sørensen <mojo11ku@hotmail.com>
2024-05-06 14:29:36 -0100

    test+wip: Deserialization test using custom converters

--- tests/SerializationTests/WebHooksTests/PaymentCreatedWebHookSerializationTests.cs
+++ tests/SerializationTests/WebHooksTests/PaymentCreatedWebHookSerializationTests.cs
@@ -16,64 +16,55 @@ namespace SolidNetsEasyClient.Tests.SerializationTests.WebHooksTests;
 [UnitTest]
 public class PaymentCreatedWebHookSerializationTests
 {
-    [Fact]
-    public void Can_deserialize_expected_string_to_PaymentCreated_object()
-    {
-        // Arrange
-        // Example from: https://developers.nets.eu/nets-easy/en-EU/api/webhooks/#created
-        const string Json = "\n" +
-            "{\n" +
-                "\"id\": \"458a4e068f454f768a40b9e576914820\",\n" +
-                "\"merchantId\": 100017120,\n" +
-                "\"timestamp\": \"2021-05-04T22:08:16.6623+02:00\",\n" +
-                "\"event\": \"payment.created\",\n" +
-                "\"data\": {\n" +
-                "\t\"order\": {\n" +
-                "\t\t\"amount\": {\n" +
-                    "\t\t\t\"amount\": 5500,\n" +
-                    "\t\t\t\"currency\": \"SEK\"\n" +
-                "\t\t},\n" +
-                "\t\t\"reference\": \"42369\",\n" +
-                "\t\t\"orderItems\": [\n" +
-                "\t\t{\n" +
-                    "\t\t\t\"reference\": \"Sneaky NE2816-82\",\n" +
-                    "\t\t\t\"name\": \"Sneaky\",\n" +
-                    "\t\t\t\"quantity\": 2,\n" +
-                    "\t\t\t\"unit\": \"pcs\",\n" +
-                    "\t\t\t\"unitPrice\": 2500,\n" +
-                    "\t\t\t\"taxRate\": 1000,\n" +
-                    "\t\t\t\"taxAmount\": 500,\n" +
-                    "\t\t\t\"netTotalAmount\": 5000,\n" +
-                    "\t\t\t\"grossTotalAmount\": 5500\n" +
-                "\t\t}\n" +
-                "\t\t]\n" +
-                "\t},\n" +
-                "\t\"paymentId\": \"02a900006091a9a96937598058c4e474\"\n" +
-                "}\n" +
-            "}\n" +
-        "";
-
-        // Act
-        var actual = JsonSerializer.Deserialize<PaymentCreated>(Json);
-
-        // Assert
-        var expected = new PaymentCreated
-        {
-            Id = new Guid("458a4e068f454f768a40b9e576914820"),
-            MerchantId = 100017120,
-            Timestamp = DateTimeOffset.Parse("2021-05-04T22:08:16.6623+02:00", CultureInfo.InvariantCulture),
-            Event = EventName.PaymentCreated,
-            Data = new()
+    const string PaymentCreatedJson = """
             {
-                Order = new()
-                {
-                    Amount = new()
-                    {
-                        Amount = 5500,
-                        Currency = Currency.SEK
+                "id": "458a4e068f454f768a40b9e576914820",
+                "merchantId": 100017120,
+                "timestamp": "2021-05-04T22:08:16.6623+02:00",
+                "event": "payment.created",
+                "data": {
+                    "order": {
+                        "amount": {
+                            "amount": 5500,
+                            "currency": "SEK"
+                        },
+                        "reference": "42369",
+                        "orderItems": [
+                            {
+                                "reference": "Sneaky NE2816-82",
+                                "name": "Sneaky",
+                                "quantity": 2,
+                                "unit": "pcs",
+                                "unitPrice": 2500,
+                                "taxRate": 1000,
+                                "taxAmount": 500,
+                                "netTotalAmount": 5000,
+                                "grossTotalAmount": 5500
+                            }
+                        ]
                     },
-                    Reference = "42369",
-                    OrderItems = new List<Item>
+                    "paymentId": "02a900006091a9a96937598058c4e474"
+                }
+            }
+        """;
+
+    private static readonly PaymentCreated PaymentCreatedExpected = new()
+    {
+        Id = new Guid("458a4e068f454f768a40b9e576914820"),
+        MerchantId = 100017120,
+        Timestamp = DateTimeOffset.Parse("2021-05-04T22:08:16.6623+02:00", CultureInfo.InvariantCulture),
+        Event = EventName.PaymentCreated,
+        Data = new()
+        {
+            Order = new()
+            {
+                Amount = new()
+                {
+                    Amount = 5500,
+                    Currency = Currency.SEK
+                },
+                Reference = "42369",
+                OrderItems = new List<Item>
                     {
                         new()
                         {
@@ -85,13 +76,21 @@ public class PaymentCreatedWebHookSerializationTests
                             TaxRate = 1000,
                         }
                     },
-                },
-                PaymentId = new Guid("02a900006091a9a96937598058c4e474")
-            }
-        };
+            },
+            PaymentId = new Guid("02a900006091a9a96937598058c4e474")
+        }
+    };
+
+    [Fact]
+    public void Can_deserialize_expected_string_to_PaymentCreated_object()
+    {
+        // Arrange
+
+        // Act
+        var actual = JsonSerializer.Deserialize<PaymentCreated>(PaymentCreatedJson);
 
         // Assert
-        actual.Should().BeEquivalentTo(expected);
+        actual.Should().BeEquivalentTo(PaymentCreatedExpected);
     }
 
     [Fact]
@@ -143,48 +142,17 @@ public class PaymentCreatedWebHookSerializationTests
     public void Deserialize_payment_response_using_JsonTypeInfo()
     {
         // Arrange
-        const string Json = """
-            {
-                "id": "458a4e068f454f768a40b9e576914820",
-                "merchantId": 100017120,
-                "timestamp": "2021-05-04T22:08:16.6623+02:00",
-                "event": "payment.created",
-                "data": {
-                    "order": {
-                        "amount": {
-                            "amount": 5500,
-                            "currency": "SEK"
-                        },
-                        "reference": "42369",
-                        "orderItems": [
-                            {
-                                "reference": "Sneaky NE2816-82",
-                                "name": "Sneaky",
-                                "quantity": 2,
-                                "unit": "pcs",
-                                "unitPrice": 2500,
-                                "taxRate": 1000,
-                                "taxAmount": 500,
-                                "netTotalAmount": 5000,
-                                "grossTotalAmount": 5500
-                            }
-                        ]
-                    },
-                    "paymentId": "02a900006091a9a96937598058c4e474"
-                }
-            }
-        """;
         var options = new JsonSerializerOptions(JsonSerializerOptions.Default);
         options.Converters.Add(new WebhookOrderConverter());
         options.Converters.Add(new PaymentCreatedDataConverter());
         options.Converters.Add(new IWebhookConverter());
-        var reader = new Utf8JsonReader(Encoding.UTF8.GetBytes(Json));
+        var reader = new Utf8JsonReader(Encoding.UTF8.GetBytes(PaymentCreatedJson));
 
         // Act
         var actual = JsonSerializer.Deserialize<IWebhook<WebhookData>>(ref reader, options);
         var paymentCreated = actual as PaymentCreated;
 
         // Assert
-        paymentCreated.Should().NotBeNull();
+        paymentCreated.Should().NotBeNull().And.BeEquivalentTo(PaymentCreatedExpected);
     }
 }
