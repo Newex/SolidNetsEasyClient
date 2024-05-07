336cf847c135e8dfcdebefbcc0153359102a3497
Johnny Kristian Moeberg Sørensen <mojo11ku@hotmail.com>
2024-05-07 22:14:21 -0100

    style: Remove unused usings

--- NetsEasyClient/Converters/ReservationCreatedV2Converter.cs
+++ NetsEasyClient/Converters/ReservationCreatedV2Converter.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 using SolidNetsEasyClient.Models.DTOs.Responses.Webhooks;
 using SolidNetsEasyClient.Models.DTOs.Responses.Webhooks.Payloads;
 
