9751e264ba5abb5aad47b60ba89abfd238bbac46
Johnny Kristian Moeberg Sørensen <mojo11ku@hotmail.com>
2024-05-07 04:40:11 -0100

    feat: PaymentCancellationFailed converter

---
+++ NetsEasyClient/Converters/PaymentCancellationFailedConverter.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using SolidNetsEasyClient.Models.DTOs.Responses.Webhooks;
+using SolidNetsEasyClient.Models.DTOs.Responses.Webhooks.Payloads;
+
+namespace SolidNetsEasyClient.Converters;
+
+/// <summary>
+/// Custom json converter
+/// </summary>
+public class PaymentCancellationFailedConverter : JsonConverter<PaymentCancellationFailed>
+{
+    /// <inheritdoc />
+    public override PaymentCancellationFailed? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var converter = new IWebhookConverter();
+        var result = converter.Read(ref reader, typeof(IWebhook<WebhookData>), options);
+        return result as PaymentCancellationFailed;
+    }
+
+    /// <inheritdoc />
+    public override void Write(Utf8JsonWriter writer, PaymentCancellationFailed value, JsonSerializerOptions options)
+    {
+        var converter = new IWebhookConverter();
+        converter.Write(writer, value, options);
+    }
+}
