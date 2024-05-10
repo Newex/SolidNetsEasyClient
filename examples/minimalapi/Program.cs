02a2fca12dc5b493bd0cce5efd8755f42cac9770
Johnny Kristian Moeberg Sørensen <mojo11ku@hotmail.com>
2024-05-10 12:11:46 -0100

    wip: Update example

--- examples/minimalapi/Program.cs
+++ examples/minimalapi/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+using MinimalAPI.Models;
 using MinimalAPI.SerializationContexts;
 using SolidNetsEasyClient.Builder;
 using SolidNetsEasyClient.Clients;
@@ -18,23 +18,25 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddNetsEasy(options =>
 {
-    options.ApiKey = "my-api-key";
     options.IntegrationType = Integration.EmbeddedCheckout;
-    options.CheckoutKey = "my-checkout-key";
-    options.CheckoutUrl = "https://localhost/checkout";
-    options.TermsUrl = "https://localhost/terms";
-    options.PrivacyPolicyUrl = "https://localhost/privacy";
+    options.CheckoutUrl = "https://localhost:5110/checkout";
+    options.TermsUrl = "https://localhost:5110/terms";
+    options.PrivacyPolicyUrl = "https://localhost:5110/privacy";
     options.ClientMode = ClientMode.Test;
     options.DefaultDenyWebhook = false;
     options.WhitelistIPsForWebhook = "127.0.0.1";
-});
+})
+.ConfigureFromConfiguration(builder.Configuration);
 
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
     options.SerializerOptions.TypeInfoResolverChain.Add(OrderSerializationContext.Default);
+    options.SerializerOptions.TypeInfoResolverChain.Add(ProductSerializationContext.Default);
 });
 
 var app = builder.Build();
+
+app.UseStaticFiles();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -43,14 +45,37 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapPost("/checkout", async (NetsPaymentBuilder builder, NetsPaymentClient client, [FromBody] Order order, CancellationToken cancellationToken) =>
+app.MapFallbackToFile("index.html");
+
+app.MapPost("/checkout", async (NetsPaymentBuilder builder, NetsPaymentClient client, Product product, CancellationToken cancellationToken) =>
 {
-    var paymentBuilder = builder.CreateSinglePayment(order, "my-order-id");
-    paymentBuilder.AddWebhook("https://localhost:8000/nets/webhook", EventName.PaymentCreated, "authHeaderVal123");
+    var order = new Order
+    {
+        Currency = product.Currency,
+        Items = [
+            new()
+            {
+                Name = product.Name,
+                Quantity = 1,
+                Reference = product.ID.ToString("N"),
+                TaxRate = 0,
+                Unit = product.Unit,
+                UnitPrice = product.Price,
+            },
+        ],
+        Reference = "my-order-id"
+    };
+    var paymentBuilder = builder.CreateSinglePayment(order, "my-payment-id");
+    paymentBuilder.AddWebhook("https://localhost:5110/nets/webhook", EventName.PaymentCreated, "authHeaderVal123");
 
     var paymentRequest = paymentBuilder.Build();
     var paymentResult = await client.StartCheckoutPayment(paymentRequest, cancellationToken);
-    return TypedResults.Created("/payment/my-order-id", paymentResult);
+
+    return TypedResults.Created("/payment/my-payment-id", new
+    {
+        CheckoutKey = client.CheckoutKey,
+        PaymentId = paymentResult?.PaymentId.ToString("N")
+    });
 });
 
 app.MapNetsWebhook("/nets/webhook", (HttpContext context, NetsPaymentClient client, IWebhook<WebhookData> payload) =>
