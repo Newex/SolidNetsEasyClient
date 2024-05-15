3965e7a2c5aad22e99ac3eac86eb729ec76a705b
Newex <mojo11ku@hotmail.com>
2024-05-15 03:02:20 -0100

    wip: Add unscheduled subscription client interface

---
+++ NetsEasyClient/Clients/IUnscheduledSubscriptionClient.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SolidNetsEasyClient.Clients;
+
+/// <summary>
+/// Bulk unscheduled subscription client.
+/// </summary>
+public interface IUnscheduledSubscriptionClient
+{
+}
